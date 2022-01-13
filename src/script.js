1e22404945b1cae257d89b3c0675c5cded3d2963
Hanna Mazur <hanna2mazur@gmail.com>
2022-01-13 17:50:55 +0100

    Add table, share-button

--- src/script.js
+++ src/script.js
@@ -1,8 +1,2 @@
-$('#navbar-collapse').on('show.bs.collapse', function() {
-    $('.nav-pills').addClass('nav-stacked');
-});
-
-//Unstack menu when not collapsed
-$('#navbar-collapse').on('hide.bs.collapse', function() {
-    $('.nav-pills').removeClass('nav-stacked');
-});
+const shareAlert = () => alert("It's the good idea to share with your friends")
+   
